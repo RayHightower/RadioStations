1a486793f07b4e9d9046bbba052d910d8d59a19f
RayHightower <rth@wisdomgroup.com>
2012-03-14 00:05:18 -0500

    created RadioStation interface and implementation files

---
+++ RadioStations/RadioStation.h
@@ -0,0 +1,13 @@
+//
+//  RadioStation.h
+//  RadioStations
+//
+//  Created by Raymond T. Hightower on 3/14/12.
+//  Copyright (c) 2012 WisdomGroup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RadioStation : NSObject
+
+@end
---
+++ RadioStations/RadioStation.m
@@ -0,0 +1,13 @@
+//
+//  RadioStation.m
+//  RadioStations
+//
+//  Created by Raymond T. Hightower on 3/14/12.
+//  Copyright (c) 2012 WisdomGroup. All rights reserved.
+//
+
+#import "RadioStation.h"
+
+@implementation RadioStation
+
+@end
