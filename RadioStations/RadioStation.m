01500b585147f1d961bc51d38f66b741900daa0d
RayHightower <rth@wisdomgroup.com>
2012-03-14 00:35:00 -0500

    RadioStation class has been created!

--- RadioStations/RadioStation.m
+++ RadioStations/RadioStation.m
@@ -10,4 +10,45 @@
 
 @implementation RadioStation
 
++(double)minAMFrequency {
+    return 520.0;
+}
+
++(double)maxAMFrequency {
+    return 1610.0;
+}
+
++(double)minFMFrequency {
+    return 88.3;
+}
+
++(double)maxFMFrequency {
+    return 107.9;
+}
+
+-(id)initWithName:(NSString *)newName atFrequency:(double)newFrequency {
+    self = [super init];
+    if (self != nil) {
+        name = newName;
+        frequency = newFrequency;
+    }
+    return self;
+}
+
+-(NSString *) name {
+    return name;
+}
+
+-(void)setName:(NSString *)newName {
+    name = newName;
+}
+
+-(double)frequency {
+    return frequency;
+}
+
+-(void)setFrequency:(double)newFrequency {
+    frequency = newFrequency;
+}
+
 @end
